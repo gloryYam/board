fd4e60f14ed627549da915c3a43fcd47ce282d6d
gloryYam <dudrhkd3892@gmail.com>
2023-12-23 20:04:27 +0900

    [test] GitHub 테스트 commit

---
+++ src/main/java/jpa/board/controller/MemberController.java
@@ -0,0 +1,15 @@
+package jpa.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+
+
+    @GetMapping("/login")
+    public String longHome() {
+        return "member/login";
+
+    }
+}
